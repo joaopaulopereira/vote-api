4a55f250a150cf5a3a88a499c4cba6cecf24375b
João Paulo <jpbetesda@gmail.com>
2022-03-04 13:11:55 -0300

    created base project

---
+++ src/test/java/com/voteapp/VoteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voteapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
